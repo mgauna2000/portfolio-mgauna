ca72074d3e52c5d761d6cc1186c6b748221ae654
Martin <martingauna2000@gmail.com>
2022-01-16 00:56:48 -0300

    empezando navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Cover from './components/Cover'
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Cover />
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Navbar.css'
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <div className='navbar-logo'>Martin</div>
+        </nav>
+    )
+}
+
+export default Navbar

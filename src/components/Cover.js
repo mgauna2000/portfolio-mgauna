95d2bc20201c1c0261f9f77a581e866786aaff4b
Martin <martingauna2000@gmail.com>
2022-01-16 00:29:58 -0300

    cover terminado

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -6,6 +6,8 @@ const Cover = () => {
     return (
         <div className='cover-container'>
             <video className='video' src={coverVideo} autoPlay loop muted />
+            <h1>Martin Gauna</h1>
+            <p>Junior Programmer | Front-end Developer</p>
         </div>
     )
 }

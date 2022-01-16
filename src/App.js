20badc1b20d5462b195179e1d44dcb40a1d79f63
Martin <martingauna2000@gmail.com>
2022-01-16 11:52:11 -0300

    terminando navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Cover from './components/Cover'
+import React, { useState, useEffect } from 'react';
+import Cover from './components/Cover';
 import './App.css';
 import Navbar from './components/Navbar';
 
 function App() {
+
+  const [scrollHeight, setScrollHeight] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollHeight(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, [setScrollHeight])
+ 
   return (
     <div className="App">
-      <Navbar />
+      <Navbar isScrolling={scrollHeight} />
       <Cover />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import '../styles/Navbar.css'
 
-function Navbar() {
+const Navbar = ({ isScrolling }) => {
+    const toTheTop = () => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth"})
+    }
     return (
-        <nav className='navbar'>
-            <div className='navbar-logo'>Martin</div>
+        <nav className={`navbar ${isScrolling > 20 ? "scrolling" : null} `}>
+            <div className='navbar-logo' onClick={toTheTop}>Martin</div>
         </nav>
     )
 }
